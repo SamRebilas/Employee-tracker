84b215e3aeb28a75398f9e4c4ceb89cd8c890786
Samrebilas <sam.rebilas@gmail.com>
2022-11-02 11:24:51 -0400

    Index and inquirer updates to all functions, as well as readme uypdate

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Employee-tracker
+
+## A command line application using mySQL to check information on employees, their roles and adding / updating employees and their roles.
+
+# Instructions
+## Clone this repo
+## Use npm i 
+## Seed the database
+## run npm start in the command line
+
+### Link to my GitHub: https://github.com/SamRebilas/Employee-tracker
+### Link to video Walk-through: 
--- index.js
+++ index.js
@@ -12,52 +12,6 @@ const db = mysql.createConnection(
   console.log("You are now logged in!")
 );
 
-
-function deleteEmployee() {
-    db.query("SELECT * FROM employee", function (err, results) {
-      const choices = results.map(({ id, first_name, last_name }) => {
-        return {
-          name: `${first_name} ${last_name}`,
-          value: id,
-        };
-      });
-  
-
-      //inquirer prompt for user delete
-      inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "employeeId",
-            message: "please choose an employee to delete.",
-            choices: choices,
-          },
-        ])
-        .then(({ employeeId }) => {
-          db.query(
-            `DELETE FROM employee WHERE id = ?`,
-            employeeId,
-            (err, result) => {
-              if (err) {
-                console.log(err)
-              }
-              console.log(result);
-              options();
-            }
-          );
-        });
-    });
-  }
-
-
-
-function showEmployees() {
-    db.query("SELECT * FROM employee", function (err, results) {
-      console.table(results)
-      options()
-    });
-  }
-  
   //inquirer prompt to view, delete, or quit the app
   function options() {
     inquirer
@@ -66,22 +20,115 @@ function showEmployees() {
           type: "list",
           name: "choice",
           message: "Please select from options below",
-          choices: ["View All Employees", "Delete Employee", "Quit"],
+          choices: ["View all Departments","View all Roles","Add a Role","View All Employees","Add an Employee", "Delete Employee", "Quit"],
         },
+      
       ])
       .then(({ choice }) => {
         switch (choice) {
-          case "View Employees":
-            showEmployees()
-            break;
+          case "View all Departments":
+          showDepartments()
+          break;
+
+          case"View all Roles":
+          showRoles()
+          break;
+
+          case "Add a Role":
+          addRole()
+          break;
+
+          case "View All Employees":
+          showEmployees()
+          break;
+
+          case "Add an Employee":
+          addEmployee()
+          break;
+
           case "Delete Employee":
-            deleteEmployee()
-            break;
+          deleteEmployee()
+          break;
+
           default:
-            process.exit()
+          process.exit()
+
         }
       })
   }
+
+//show departments function
+  function showDepartments(){
+    db.query("SELECT * FROM department", function (err, results){
+      console.table(results)
+      options()
+    })
+
+  }
+//show rall roles
+function showRoles(){
+  db.query("SELECT * FROM roles", function (err, results){
+    console.table(results)
+    options()
+  })
+}
+//add a role
+function addRole(){
+
+}
+//show all employees
+function showEmployees() {
+  db.query("SELECT * FROM employee", function (err, results) {
+    console.table(results)
+    options()
+  });
+}
+// add an employee
+function addEmployee(){
+  inquirer.prompt({
+    type: "input",
+    name: "EmployeeName",
+    message: "What is your employees name?"
+
+})
+}
+
+// delete an employee
+function deleteEmployee() {
+  db.query("SELECT * FROM employee", function (err, results) {
+    const choices = results.map(({ id, first_name, last_name }) => {
+      return {
+        name: `${first_name} ${last_name}`,
+        value: id,
+      };
+    });
+
+
+    //inquirer prompt for user delete
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "please choose an employee to delete.",
+          choices: choices,
+        },
+      ])
+      .then(({ employeeId }) => {
+        db.query(
+          `DELETE FROM employee WHERE id = ?`,
+          employeeId,
+          (err, result) => {
+            if (err) {
+              console.log(err)
+            }
+            console.log(result);
+            options();
+          }
+        );
+      });
+  });
+}
 
 
 
